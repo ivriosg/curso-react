6f083e5bc4786c0014ec8136eb24019b2e5c0e7e
ivriosg <ivriosg@gmail.com>
2020-10-23 00:50:41 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import Producto from "./components/Producto";
+import Carrito from "./components/Carrito";
 import Footer from "./components/Footer";
 
 function App() {
-  //crear listado de productos
-  //utilizamos useState
+  //crear listado de productos -- utilizamos useState
   const [productos, guardarProductos] = useState([
     { id: 1, nombre: "Camisa ReactJS", precio: 50 },
     { id: 2, nombre: "Camisa AngularJS", precio: 40 },
@@ -13,6 +13,9 @@ function App() {
     { id: 4, nombre: "Camisa VueJS", precio: 20 },
     { id: 5, nombre: "Camisa PHP", precio: 10 },
   ]);
+
+  //state para carrito de compras
+  const [carrito, agregarProducto] = useState([]);
 
   //obtener la fecha
   const fecha = new Date().getFullYear();
@@ -22,10 +25,19 @@ function App() {
       <Header titulo="Tienda Virtual" />
       <h1>Lista de productos</h1>
       {productos.map((producto) => (
-        <Producto 
-        key={producto.id} 
-        producto={producto} />
+        <Producto
+          key={producto.id}
+          producto={producto}
+          productos={productos}
+          carrito={carrito}
+          agregarProducto={agregarProducto}
+        />
       ))}
+
+      <Carrito 
+        carrito={carrito}
+        agregarProducto={agregarProducto}
+      />
 
       <Footer fecha={fecha} />
     </Fragment>
--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,11 +1,40 @@
 import React from "react";
-const Producto = ({producto}) => {
+const Producto = ({ producto, carrito, agregarProducto, productos }) => {
+  const { nombre, precio, id } = producto;
 
-  const { nombre, precio, id } = producto
+  //agregar producto al carrito
+  const seleccionarProducto = (id) => {
+    const producto = productos.filter((producto) => producto.id === id)[0];
+    agregarProducto([...carrito, producto]);
+  };
+
+  //eliminar producto 
+  const eliminarProducto = id => {
+    const productos = carrito.filter(producto => producto.id !== id);
+
+
+    //colocar producos en el state
+    agregarProducto(productos)
+  }
+
   return (
     <div>
       <h2>{nombre}</h2>
       <p>${precio}</p>
+      { productos 
+      ? 
+        (
+          <button type="button" onClick={() => seleccionarProducto(id)}>
+            Comprar
+          </button>
+        ) 
+      : 
+        (
+          <button type="button" onClick={() => eliminarProducto(id)}>
+            Eliminar
+          </button>
+        )
+      }
     </div>
   );
 };
