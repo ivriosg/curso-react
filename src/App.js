46686d98133c86c4f2d36ee97daea4a396778ebc
ivriosg <ivriosg@gmail.com>
2020-10-22 23:43:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import Header from "./components/Header";
+import Producto from "./components/Producto";
+import Footer from "./components/Footer";
 
 function App() {
+  //crear listado de productos
+  //utilizamos useState
+  const [productos, guardarProductos] = useState([
+    { id: 1, nombre: "Camisa ReactJS", precio: 50 },
+    { id: 2, nombre: "Camisa AngularJS", precio: 40 },
+    { id: 3, nombre: "Camisa NodeJS", precio: 30 },
+    { id: 4, nombre: "Camisa VueJS", precio: 20 },
+    { id: 5, nombre: "Camisa PHP", precio: 10 },
+  ]);
+
+  //obtener la fecha
+  const fecha = new Date().getFullYear();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo="Tienda Virtual" />
+      <h1>Lista de productos</h1>
+      {productos.map((producto) => (
+        <Producto 
+        key={producto.id} 
+        producto={producto} />
+      ))}
+
+      <Footer fecha={fecha} />
+    </Fragment>
   );
 }
 
